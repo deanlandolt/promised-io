38259fc7ca11bdcdb8ab3e3519d4766d8ae7e023
Dean Landolt <dean@deanlandolt.com>
2011-04-23 16:18:11 -0400

    major refactor of process module

---
+++ tests/process.js
@@ -0,0 +1,35 @@
+var test = require('patr');
+var assert = test.assert;
+var when = require('../lib/promise').when;
+var process = require('../lib/process');
+
+exports.testOutput = {
+    testPrint: function() {
+        return when(process.print('a', 0, ['b', null, 1], {}), function(value) {
+            assert.equal(value, 'a 0 b,,1 [object Object]\n');
+        });
+    },
+    testPuts: function() {
+        return when(process.puts('a', 0, ['b', null, 1], {}), function(value) {
+            assert.equal(value, 'a\n0\nb,,1\n[object Object]\n');
+        });
+    },
+    testProperties: function() {
+        assert.equal(typeof process.userId, 'number');
+        assert.equal(typeof process.groupId, 'number');
+        var memory = process.memory;
+        var keys = Object.keys(memory).sort();
+        assert.deepEqual(keys, ['heapTotal', 'heapUsed', 'rss', 'vsize']);
+        keys.forEach(function(key) {
+            assert.equal(typeof memory[key], 'number');
+        });
+        
+        //assert.equal(typeof process.global, 'object');
+        assert.equal(typeof process.env, 'object');
+        assert.ok(Array.isArray(process.args));
+    }
+    
+    // TODO use a child process to test
+};
+
+if (require.main === module) test.run(exports);
